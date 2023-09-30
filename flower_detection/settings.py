49b45aecf86a5f2afa5a8ba9c32b9ce72a0db256
DeEpRaJ <pagaredeepraj05@gmail.com>
2023-09-30 21:55:09 +0530

    Update settings.py

--- flower_detection/settings.py
+++ flower_detection/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'detect/static/'
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
